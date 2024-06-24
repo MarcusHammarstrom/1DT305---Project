df6ba801bb27bc3396542b3acd9a1634276d6d26
MarcusHammarstrom <marham1995@gmail.com>
2024-06-24 13:40:28 +0200

    Testing moisture sensors

--- src/main.py
+++ src/main.py
@@ -4,13 +4,20 @@ from machine import Pin, ADC
 import time
 
 LED_PIN = 2
-SENSOR_PIN = 34
+PARSLEY_PIN = 35
+OREGANO_PIN = 34
 
-led = Pin(2, Pin.OUT)
-sensor = ADC(Pin(SENSOR_PIN))
-sensor.atten(ADC.ATTN_11DB)
+led = Pin(LED_PIN, Pin.OUT)
+oregano_sensor = ADC(Pin(OREGANO_PIN))
+oregano_sensor.atten(ADC.ATTN_11DB)
+parsley_sensor = ADC(Pin(PARSLEY_PIN))
+parsley_sensor.atten(ADC.ATTN_11DB)
 
 while True:
+    oregano_val = oregano_sensor.read()
+    parsley_val = parsley_sensor.read()
+    print("Oregano value: " + str(oregano_val))
+    print("Parsley value: " + str(parsley_val))
     led.value(1)
     time.sleep(1)
     led.value(0)
