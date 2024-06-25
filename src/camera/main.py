865592b6e1564a6b02f59a17efb630ff3149066a
MarcusHammarstrom <marham1995@gmail.com>
2024-06-25 23:22:27 +0200

    camera test

--- src/camera/main.py
+++ src/camera/main.py
@@ -1,4 +1,19 @@
 # main.py -- put your code here!
 
-from machine import Pin
+import camera
 import utime
+
+camera.init(0, format=camera.JPEG)
+
+while True:
+    print("Taking a photo in 5 seconds")
+    utime.sleep(5)
+    try: 
+        buffer = camera.capture()
+        print("Size of image is " + len(buffer) + "bytes")
+        file_path = "img.jpg"
+        file = open(file_path, "w")
+        file.write(buffer)
+        file.close()
+    except:
+        print("Something went wrong")
