639a8c4839adace4c0cb4216cbf7ca0dc69f9807
Marcus Hammarström <marham1995@gmail.com>
2024-06-30 14:00:28 +0200

    Calibrating soil moisture sensors

--- src/controller/main.py
+++ src/controller/main.py
@@ -5,18 +5,18 @@ import utime
 import secrets
 import urequests as requests
 
-MIN_OREGANO = 0
-MIN_PARSLEY = 0
-MAX_OREGANO = 0
-MAX_PARSLEY = 0
+MIN_CHIVES = 0
+MIN_DILL = 0
+MAX_CHIVES = 0
+MAX_DILL = 0
 
-PARSLEY_PIN = 34
-OREGANO_PIN = 35
+DILL_PIN = 32
+CHIVES_PIN = 35
 
-oregano_sensor = ADC(Pin(OREGANO_PIN))
-oregano_sensor.atten(ADC.ATTN_11DB)
-parsley_sensor = ADC(Pin(PARSLEY_PIN))
-parsley_sensor.atten(ADC.ATTN_11DB)
+chives_sensor = ADC(Pin(CHIVES_PIN))
+chives_sensor.atten(ADC.ATTN_11DB)
+dill_sensor = ADC(Pin(DILL_PIN))
+dill_sensor.atten(ADC.ATTN_11DB)
 
 def BuildJSON(value):
     data = { "value": value }
@@ -29,23 +29,22 @@ def SendData(value, feed):
 
 while True:
     try:
-        sum_parsley = 0
-        sum_oregano = 0
+        sum_dill = 0
+        sum_chives = 0
+        print("Making sensor readings")
         for i in range(10):
             utime.sleep(0.5)
-            temp_oregano = oregano_sensor.read()
-            temp_parsley = parsley_sensor.read()
+            temp_chives = chives_sensor.read()
+            temp_dill = dill_sensor.read()
 
-            sum_oregano += temp_oregano
-            sum_parsley += temp_parsley
-            print("Oregano value: " + str(temp_oregano))
-            print("Parsley value: " + str(temp_parsley))
-        oregano_val = sum_oregano / 15.0
-        parsley_val = sum_parsley / 15.0
-        print("Avaraged Oregano value: " + str(oregano_val))
-        print("Aaraged Parsley value: " + str(parsley_val))
-        #SendData(str(oregano_val), secrets.AIO_PLANT1_FEED)
-        #SendData(str(parsley_val), secrets.AIO_PLANT2_FEED)
+            sum_chives += temp_chives
+            sum_dill += temp_dill
+        chives_val = sum_chives / 10.0
+        dill_val = sum_dill / 10.0
+        print("Chives value: " + str(chives_val))
+        print("Dill value: " + str(dill_val))
+        #SendData(str(dill_val), secrets.AIO_PLANT1_FEED)
+        #SendData(str(chives_val), secrets.AIO_PLANT2_FEED)
     except Exception as e:
         print("Something went wrong")
         print(str(e))
